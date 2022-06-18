fd03721a70fe68898688b8e5c6bc28758c336078
Danila Khnykin <khnykin@redcollar.ru>
2022-06-18 05:44:19 +0300

    Added REST for part entities

--- src/main/java/ru/cs/vsu/ast2_backend/repository/PartRepository.java
+++ src/main/java/ru/cs/vsu/ast2_backend/repository/PartRepository.java
@@ -3,11 +3,9 @@ package ru.cs.vsu.ast2_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.cs.vsu.ast2_backend.model.entity.part.PartEntity;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface PartRepository extends JpaRepository<PartEntity, UUID> {
 
-    List<PartEntity> findAllByBrandId(UUID brandId);
 
 }
